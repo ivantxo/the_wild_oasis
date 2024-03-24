eae22445be3ed9e556c930022c0bdb718da4258a
IvanR <ivan.ramirezf@gmail.com>
2024-03-25 05:15:34 +1000

    Styled component basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return <H1>The Wild Oasis</H1>;
 }
 
 export default App;
