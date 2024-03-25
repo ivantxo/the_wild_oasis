6b8e4e8ff77d3fc552654c85d1adcd1caeb5e8d4
IvanR <ivan.ramirezf@gmail.com>
2024-03-26 05:29:06 +1000

    Adding Outlet in AppLayout
    - Outlet allows child pages to be part of parent page

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function AppLayout() {
-  return <div>APP LAYOUT</div>;
+  return (
+    <div>
+      <p>APP LAYOUT</p>
+      <Outlet />
+    </div>
+  );
 }
 
 export default AppLayout;
