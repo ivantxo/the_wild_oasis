b6f3c1c9226d297618fb97ccdf4c5baf3720318a
IvanR <ivan.ramirezf@gmail.com>
2024-03-27 04:22:17 +1000

    Styled components in Header, Sidebar and AppLayout

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
+const StyledSideBar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+  grid-row: 1 / -1;
+`;
+
 function SideBar() {
-  return <aside>SIDE BAR</aside>;
+  return <StyledSideBar>SIDE BAR</StyledSideBar>;
 }
 
 export default SideBar;
