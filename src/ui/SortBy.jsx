5a445adedbc0e411ee3c9b9b836f775fab3f3bc0
IvanR <ivan.ramirezf@gmail.com>
2024-04-18 05:21:32 +1000

    Sort II
    - Add type to Select using speading props technique

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,7 +1,7 @@
 import Select from "./Select";
 
 function SortBy({ options }) {
-  return <Select options={options} />;
+  return <Select options={options} type="white" />;
 }
 
 export default SortBy;
